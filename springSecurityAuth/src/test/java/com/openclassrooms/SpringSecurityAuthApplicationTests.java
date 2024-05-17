3bb7f7d0f390d9511576a635e5e5f3785783ceb8
77madel <madoumadeltitokone77@gmail.com>
2024-05-17 16:36:18 +0000

    pour tester les codes

--- springSecurityAuth/src/test/java/com/openclassrooms/SpringSecurityAuthApplicationTests.java
+++ springSecurityAuth/src/test/java/com/openclassrooms/SpringSecurityAuthApplicationTests.java
@@ -1,13 +1,20 @@
 package com.openclassrooms;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.openclassrooms.controller.LoginController;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class SpringSecurityAuthApplicationTests {
 
+	@Autowired
+	private LoginController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads()throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
